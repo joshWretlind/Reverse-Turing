8b78ce6d2df68bcc3561ca30863dcd0af3de69b1
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-01-20 16:57:08 -0700

    Added a class to calculate the fequency of strings in a collection in linear time

---
+++ src/com/core/string/algo/Histogram.java
@@ -0,0 +1,26 @@
+package com.core.string.algo;
+
+/**
+ * Creates a fequency map for a given collection of strings
+ */
+import java.util.HashMap;
+import java.util.Map;
+
+public class Histogram {
+	
+	public static Map<String, Integer> create(String... sc){
+		Map<String, Integer> map = new HashMap<String,Integer>();
+		int i = 1;
+		for(String s: sc){
+			if(!map.containsKey(s.toLowerCase())){
+				map.put(s.toLowerCase(),i);
+			}
+			else{
+				map.put(s.toLowerCase(), map.get(s.toLowerCase())+1);
+			}
+		}
+		
+		return map;
+	}
+	
+}
